34f2444b1dd17238c5fc0bbb0d925023aa27dd27
Raffael Cabral <raffa_cabral@hotmail.com>
2023-07-08 16:48:17 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/raffael/pomodoro/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.raffael.pomodoro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/store/contador.store.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pomodoro Timer',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+final store = ContadorStore();
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(
+              builder: (_) {
+                return Text(
+                  '${store.contador}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: store.incrementar,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), //
+    );
+  }
+}
