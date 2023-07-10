9bffebb78c684f61834b075818134f7920a244f3
Raffael Cabral <raffa_cabral@hotmail.com>
2023-07-10 14:31:48 -0300

    ensure reset function

--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -1,10 +1,14 @@
 // ignore_for_file: library_private_types_in_public_api
 
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 
 part 'pomodoro.store.g.dart';
 
 class PomodoroStore = _PomodoroStore with _$PomodoroStore;
+
+enum TipoIntervalo { trabalho, descanso }
 
 abstract class _PomodoroStore with Store {
   @observable
@@ -22,19 +26,36 @@ abstract class _PomodoroStore with Store {
   @observable
   int tempoDescanso = 0;
 
+  @observable
+  TipoIntervalo tipoIntervalo = TipoIntervalo.descanso;
+
   @action
   void iniciar() {
     iniciado = true;
+    cronometro = Timer.periodic(
+      const Duration(milliseconds: 50),
+      (timer) {
+        if (minutos == 0 && segundos == 0) {
+          _trocarTipoIntervalo();
+        } else if (segundos == 0) {
+          segundos = 59;
+          minutos--;
+        } else {
+          segundos--;
+        }
+      },
+    );
   }
 
   @action
   void parar() {
     iniciado = false;
+    cronometro?.cancel();
   }
 
   @action
   void reiniciar() {
-    iniciado = false;
+    parar();
   }
 
   @action
@@ -55,5 +76,26 @@ abstract class _PomodoroStore with Store {
   @action
   void decrementarTempoDescanso() {
     tempoDescanso--;
+  }
+
+  bool estaTrabalhando() {
+    return tipoIntervalo == TipoIntervalo.trabalho;
+  }
+
+  bool estaDescansando() {
+    return tipoIntervalo == TipoIntervalo.descanso;
+  }
+
+  Timer? cronometro;
+
+  void _trocarTipoIntervalo() {
+    if (estaTrabalhando()) {
+      tipoIntervalo = TipoIntervalo.descanso;
+      minutos = tempoDescanso;
+    } else {
+      tipoIntervalo = TipoIntervalo.trabalho;
+      minutos = tempoTrabalho;
+    }
+    segundos = 0;
   }
 }
